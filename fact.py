cf008612be1f23797f4585f917fb6f38658c6f1a
Mariana <mariana.yuste.rz@gmail.com>
2023-08-22 18:11:54 -0600

    fix: :green_heart: ahora pa todos >:)

--- fact.py
+++ fact.py
@@ -1,8 +1,6 @@
 def fact(num):
-    #comentario para la rama 1 <3 uwu
+    #ahora si para cualquier factorial
     if num == 1:
         return 1
-    elif num == 2:
-        return 2
-    elif num == 3:
-        return 6
+    else:
+        return num * fact(num-1)
