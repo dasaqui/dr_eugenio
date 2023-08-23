25e8957ac2754522763ee280848f7cc2301d9ff1
Leticia <leticia.abrica@gmail.com>
2023-08-22 18:11:50 -0600

    feat: :fire: se añadio el else

--- fact.py
+++ fact.py
@@ -2,7 +2,6 @@ def fact(num):
     # Este es un comentario ... jajaja
     if num == 1:
         return 1
-    elif num == 2:
-        return 2
-    elif num == 3:
-        return 6
+    else:
+        return num * fact(num -1)
+    
