776cc3aa42712f024b91b019caa44374206beec8
DanielS <daniel@amphorahealth.com>
2023-08-22 18:11:48 -0600

    fix: :art: implement code to estimate all factorials

--- fact.py
+++ fact.py
@@ -2,7 +2,5 @@ def fact( num):
     # This code is to calculate the factorial of a number
     if num == 1:
         return 1
-    elif num == 2:
-        return 2
-    elif num == 3:
-        return 6
+    else:
+        return num * fact( num - 1)
