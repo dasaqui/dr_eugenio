b87c8a67dc955fd8a2097a285462fd55a5096138
david <davidga_86@ciencias.unam.mx>
2023-08-22 18:12:31 -0600

    perf:

--- fact.py
+++ fact.py
@@ -2,7 +2,5 @@ def fact( num):
     # este es un comentario dah!
     if num == 1:
         return 1
-    elif num == 2:
-        return 2
-    elif num == 3:
-        return 6
+    else:
+        return num * fact( num - 1)
