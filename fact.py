ed17392dc170eaeef84cdddf730d67c47ebd881b
eugenio azpeitia <eazpeitia@matmor.unam.mx>
2023-08-22 18:11:48 -0600

    perf:

--- fact.py
+++ fact.py
@@ -2,9 +2,5 @@ def fact( num):
     #aqui se calcula el factorial
     if num==1:
         return1
-    elif num==2:
-        return 2
-    elif num==3:
-        return 6
     else:
-        return 'too hard to compute'
+        return num * fact(num-1)
